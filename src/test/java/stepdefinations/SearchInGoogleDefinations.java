1191c6bfc502fb633430741a85e26dd5ef5d6916
Administrator <Administrator@172.31.4.29>
2024-02-12 17:13:42 +0000

    diff scenarios

--- src/test/java/stepdefinations/SearchInGoogleDefinations.java
+++ src/test/java/stepdefinations/SearchInGoogleDefinations.java
@@ -1,26 +1,37 @@
 package stepdefinations;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class SearchInGoogleDefinations {
-	@Given("navigate to google page")
-	public void navigate()
-	{
-		System.out.println("on google page");
-		}
-	@When("I click search and enter")
-	public void clicksearch()
-	{
-		System.out.println("searching...");
-		}
-
-	@Then("I should redirect to search result page")
-	public void redirect()
-	{
-		System.out.println("redirected to search page");
-		}
-
-	
+	/*
+	 * WebDriver driver=new ChromeDriver();
+	 * 
+	 * @Given("user is on google page") public void user_is_on_google_page() {
+	 * driver.get("https://www.google.com/"); }
+	 * 
+	 * @When("User search java tut") public void user_search_java_tut() { WebElement
+	 * search=driver.findElement(By.xpath("//*[@id=\"APjFqb\"]"));
+	 * search.sendKeys("java"); }
+	 * 
+	 * @Then("I should display java result page") public void
+	 * i_should_display_java_result_page() { System.out.println("in java page"); }
+	 * 
+	 * @Given("user is on selenium page") public void user_is_on_selenium_page() {
+	 * driver.get("https://www.google.com/"); }
+	 * 
+	 * @When("User search selenium tut") public void user_search_selenium_tut1() {
+	 * WebElement search=driver.findElement(By.xpath("//*[@id=\"APjFqb\"]"));
+	 * search.sendKeys("selenium"); }
+	 * 
+	 * @Then("I should display selenium result page") public void
+	 * i_should_display_selenium_result_page() {
+	 * System.out.println("in selenium page"); }
+	 */
 }
